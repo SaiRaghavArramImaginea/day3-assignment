98037d94033952f50b0b324d6d0612d25fd55e19
SaiRaghavArramSema4 <sairaghav.arram@sema4genomics.com>
2020-06-24 11:01:54 +0530

    Initial commit

---
+++ src/main/java/com/pramati/profilemanagement/ProfileUtility.java
@@ -0,0 +1,14 @@
+package com.pramati.profilemanagement;
+
+import java.util.Map;
+
+public class ProfileUtility {
+	
+	
+	public static Map<String, Map<String, Integer>> operationsCount(Object obj) {
+		
+		
+		return null;
+	}
+
+}
